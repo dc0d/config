dad2056ad6cc5339c431c78e1aff47918a4f2c80
dc0d <kaveh.shahbazian@gmail.com>
2018-01-14 10:12:50 +0330

    RelativeResource in extra dirs

--- config_test.go
+++ config_test.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,4 +13,22 @@ func Test01(t *testing.T) {
 	p, err := RelativeResource("functions.go")
 	assert.NoError(err)
 	assert.Contains(p, "/src/github.com/dc0d/config/functions.go")
+}
+
+func Test02(t *testing.T) {
+	assert := assert.New(t)
+
+	p, err := RelativeResource("app.yml", "./ymlconfig")
+	assert.NoError(err)
+	assert.Contains(p, "/src/github.com/dc0d/config/ymlconfig/app.yml")
+}
+
+func Test03(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.NoError(os.Chdir("./hclconfig"))
+
+	p, err := RelativeResource("app.yml", "./../ymlconfig")
+	assert.NoError(err)
+	assert.Contains(p, "/src/github.com/dc0d/config/ymlconfig/app.yml")
 }
--- functions.go
+++ functions.go
@@ -7,35 +7,78 @@ import (
 
 //-----------------------------------------------------------------------------
 
-// RelativeResource returns the file path 1 - if it's inside the directory of the program
-// 2 - if it's inside the working directory 3 - returns os.ErrNotExist error
-func RelativeResource(fileName string) (filePath string, funcErr error) {
-	appDir, err := os.Executable()
-	if err != nil {
-		funcErr = err
+/*
+RelativeResource returns the file path
+
+	1 - searchs inside dirs, in the same order of dirs
+	2 - searchs inside working directory
+	3 - searchs inside app directory
+
+otherwise returns os.ErrNotExist error
+*/
+func RelativeResource(fileName string, dirs ...string) (fp string, fe error) {
+	if fp, fe = searchDirs(fileName, dirs...); fe == nil {
 		return
 	}
-	appDir, err = filepath.Abs(filepath.Dir(appDir))
-	if err != nil {
-		funcErr = err
+	if fp, fe = searchWD(fileName); fe == nil {
+		return
+	}
+	if fp, fe = searchAppDir(fileName); fe == nil {
+		return
+	}
+	fe = os.ErrNotExist
+	return
+}
+
+func searchDirs(fileName string, dirs ...string) (fp string, fe error) {
+	for _, v := range dirs {
+		d, err := filepath.Abs(v)
+		if err != nil {
+			continue
+		}
+		p := filepath.Join(d, fileName)
+		if _, fe = os.Stat(p); fe == nil {
+			fp = p
+			return
+		}
+	}
+	fe = os.ErrNotExist
+	return
+}
+
+func searchAppDir(fileName string) (fp string, fe error) {
+	var (
+		appDir string
+	)
+	appDir, fe = os.Executable()
+	if fe != nil {
+		return
+	}
+	appDir, fe = filepath.Abs(filepath.Dir(appDir))
+	if fe != nil {
 		return
 	}
 	p := filepath.Join(appDir, fileName)
-	if _, err := os.Stat(p); err == nil {
-		filePath = p
+	if _, fe = os.Stat(p); fe == nil {
+		fp = p
 		return
 	}
-	wd, err := os.Getwd()
-	if err != nil {
-		funcErr = err
+	return
+}
+
+func searchWD(fileName string) (fp string, fe error) {
+	var (
+		wd string
+	)
+	wd, fe = os.Getwd()
+	if fe != nil {
 		return
 	}
-	p = filepath.Join(wd, fileName)
-	if _, err := os.Stat(p); err == nil {
-		filePath = p
+	p := filepath.Join(wd, fileName)
+	if _, fe = os.Stat(p); fe == nil {
+		fp = p
 		return
 	}
-	funcErr = os.ErrNotExist
 	return
 }
 
